8cf1e3f1e5bbe210a18a226ca6b13bbbcd36de32
Sean Steel <seanrsteel@gmail.com>
2020-10-23 11:31:53 -0600

    Create test for adding a ride to a mechanic:

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,5 +1,8 @@
 class Mechanic < ApplicationRecord
   has_many :mechanic_rides
   has_many :rides, through: :mechanic_rides
-  
+
+  def add_ride(ride)
+    self.rides << ride
+  end
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Mechanic, type: :model do
     it { should have_many :mechanic_rides}
     it { should have_many(:rides).through(:mechanic_rides)}
   end
+
+  describe 'instance methods' do
+    it '#add_ride' do
+      park = Park.create(name: 'Hershey Park', price: 50)
+      ride1 = Ride.create(name: 'Lightning Racer', rating: 9, park_id: park.id)
+      ride2 = Ride.create(name: 'Storm Runner', rating: 6, park_id: park.id)
+      ride3 = Ride.create(name: 'The Great Bear', rating: 8, park_id: park.id)
+      mechanic = Mechanic.create!(name: 'Sam Mills', experience: 10)
+      mech_ride1 = MechanicRide.create(ride_id: ride1.id, mechanic_id: mechanic.id)
+      mech_ride2 = MechanicRide.create(ride_id: ride2.id, mechanic_id: mechanic.id)
+      mech_ride3 = MechanicRide.create(ride_id: ride3.id, mechanic_id: mechanic.id)
+
+      expect(mechanic.rides).to eq([ride1, ride2, ride3])
+
+      ride4 = Ride.create(name: 'The Kiss Raise', rating: 3, park_id: park.id)
+      mechanic.add_ride(ride4)
+
+      expect(mechanic.rides).to eq([ride1, ride2, ride3, ride4])
+    end
+  end
 end
